2f173e81398848991d11e1c405fa64b701fb6c3a
Conrad Kleinn <kleinnconrad@gmail.com>
2026-04-24 15:44:26 +0200

    Refactor offline solder plan for 30-pin design

--- schaltplan/schaltplan_ohne_lte.py
+++ schaltplan/schaltplan_ohne_lte.py
@@ -1,31 +1,31 @@
 import graphviz
 
-def erstelle_perfekten_offline_loetplan():
-    dot = graphviz.Digraph('Offline_Schaltplan', filename='Schaltplan_Offline_Perfekt', format='png')
+def erstelle_perfekten_offline_loetplan_30pin():
+    dot = graphviz.Digraph('Offline_Schaltplan', filename='Schaltplan_Offline_30Pin', format='png')
     
     # rankdir='LR' erzwingt den horizontalen Fluss.
     dot.attr(rankdir='LR', splines='polyline', nodesep='1.0', ranksep='4.0')
     dot.attr('node', shape='none', fontname='Helvetica', fontsize='12')
 
-    # 1. ESP32 ZENTRALE
+    # 1. ESP32 ZENTRALE (Jetzt im korrekten 30-Pin Design!)
     esp32_html = '''<
     <TABLE BORDER="1" CELLBORDER="1" CELLSPACING="0" CELLPADDING="6">
-      <TR><TD COLSPAN="3" BGCOLOR="#add8e6"><B>NodeMCU ESP32 (Hub)</B></TD></TR>
+      <TR><TD COLSPAN="3" BGCOLOR="#add8e6"><B>ESP32 30-Pin (FREENOVE Board)</B></TD></TR>
       <TR><TD BGCOLOR="#d3d3d3"><B>Linke Pins</B></TD><TD BGCOLOR="#e0e0e0" ROWSPAN="11"> ESP32 <br/> Core </TD><TD BGCOLOR="#d3d3d3"><B>Rechte Pins</B></TD></TR>
-      <TR><TD PORT="vin">VIN (5V In)</TD><TD PORT="3v3_r1">3.3V (Out)</TD></TR>
-      <TR><TD PORT="gnd_l1">GND (Masse)</TD><TD PORT="gnd_r1">GND (Masse)</TD></TR>
-      <TR><TD PORT="g32">GPIO 32 (Frei)</TD><TD PORT="g16">GPIO 16 (RX2)</TD></TR>
-      <TR><TD PORT="g33">GPIO 33 (Frei)</TD><TD PORT="g17">GPIO 17 (TX2)</TD></TR>
-      <TR><TD PORT="3v3_l">3.3V (Out)</TD><TD PORT="3v3_r2">3.3V (Out)</TD></TR>
-      <TR><TD PORT="gnd_l2">GND (Masse)</TD><TD PORT="gnd_r2">GND (Masse)</TD></TR>
-      <TR><TD PORT="g23">GPIO 23 (MOSI)</TD><TD PORT="g4">GPIO 4 (1-Wire)</TD></TR>
-      <TR><TD PORT="g19">GPIO 19 (MISO)</TD><TD PORT="g2">GPIO 2 (Interrupt)</TD></TR>
-      <TR><TD PORT="g18">GPIO 18 (SCK)</TD><TD>---</TD></TR>
-      <TR><TD PORT="g5">GPIO 5 (CS)</TD><TD>---</TD></TR>
+      <TR><TD PORT="vin">VIN (5V In)</TD><TD PORT="3v3">3V3 (3.3V Out)</TD></TR>
+      <TR><TD PORT="gnd_l">GND (Masse)</TD><TD PORT="gnd_r">GND (Masse)</TD></TR>
+      <TR><TD>---</TD><TD PORT="d2">D2 (Hall-Sensor)</TD></TR>
+      <TR><TD>---</TD><TD PORT="d4">D4 (1-Wire Temp)</TD></TR>
+      <TR><TD>---</TD><TD PORT="rx2">RX2 / GPIO16</TD></TR>
+      <TR><TD>---</TD><TD PORT="tx2">TX2 / GPIO17</TD></TR>
+      <TR><TD>---</TD><TD PORT="d5">D5 (SD CS)</TD></TR>
+      <TR><TD>---</TD><TD PORT="d18">D18 (SD SCK)</TD></TR>
+      <TR><TD>---</TD><TD PORT="d19">D19 (SD MISO)</TD></TR>
+      <TR><TD>---</TD><TD PORT="d23">D23 (SD MOSI)</TD></TR>
     </TABLE>>'''
     dot.node('ESP', label=esp32_html)
 
-    # 2. KOMPONENTEN LINKE SEITE (Jetzt mit RC-Empfänger)
+    # 2. KOMPONENTEN LINKE SEITE (Nur noch Stromversorgung)
     rx_html = '''<
     <TABLE BORDER="1" CELLBORDER="1" CELLSPACING="0" CELLPADDING="6">
       <TR><TD BGCOLOR="#90ee90"><B>RC Empfänger (BEC)</B></TD></TR>
@@ -34,10 +34,15 @@ def erstelle_perfekten_offline_loetplan():
       <TR><TD PORT="signal">Signal (Nicht genutzt)</TD></TR>
     </TABLE>>'''
 
+    with dot.subgraph() as s_left:
+        s_left.attr(rank='same')
+        s_left.node('RX', label=rx_html)
+
+    # 3. KOMPONENTEN RECHTE SEITE (Alle Sensoren + SD Karte)
     sd_html = '''<
     <TABLE BORDER="1" CELLBORDER="1" CELLSPACING="0" CELLPADDING="6">
       <TR><TD BGCOLOR="#d3d3d3"><B>MicroSD (SPI)</B></TD></TR>
-      <TR><TD PORT="vcc">VCC (3.3V)</TD></TR>
+      <TR><TD PORT="vcc">VCC (Zwingend an 5V!)</TD></TR>
       <TR><TD PORT="gnd">GND</TD></TR>
       <TR><TD PORT="mosi">MOSI</TD></TR>
       <TR><TD PORT="miso">MISO</TD></TR>
@@ -45,17 +50,10 @@ def erstelle_perfekten_offline_loetplan():
       <TR><TD PORT="cs">CS</TD></TR>
     </TABLE>>'''
 
-    with dot.subgraph() as s_left:
-        s_left.attr(rank='same')
-        s_left.node('RX', label=rx_html)
-        s_left.node('SD', label=sd_html)
-        s_left.edge('RX', 'SD', style='invis')
-
-    # 3. KOMPONENTEN RECHTE SEITE (Unverändert)
     gps_html = '''<
     <TABLE BORDER="1" CELLBORDER="1" CELLSPACING="0" CELLPADDING="6">
       <TR><TD BGCOLOR="#87cefa"><B>GPS BN-220</B></TD></TR>
-      <TR><TD PORT="vcc">VCC (3.3V)</TD></TR>
+      <TR><TD PORT="vcc">VCC (3.3V via WAGO)</TD></TR>
       <TR><TD PORT="gnd">GND</TD></TR>
       <TR><TD PORT="tx">TX (Senden)</TD></TR>
       <TR><TD PORT="rx">RX (Empfang)</TD></TR>
@@ -64,23 +62,23 @@ def erstelle_perfekten_offline_loetplan():
     temp_mot_html = '''<
     <TABLE BORDER="1" CELLBORDER="1" CELLSPACING="0" CELLPADDING="6">
       <TR><TD BGCOLOR="#ffffe0"><B>DS18B20 (Motor)</B></TD></TR>
-      <TR><TD PORT="vcc">VDD (Rot)</TD></TR>
-      <TR><TD PORT="gnd">GND (Schwarz)</TD></TR>
+      <TR><TD PORT="vcc">VDD (3.3V via WAGO)</TD></TR>
+      <TR><TD PORT="gnd">GND</TD></TR>
       <TR><TD PORT="dq">Data (Gelb/Blau)</TD></TR>
     </TABLE>>'''
 
     temp_esc_html = '''<
     <TABLE BORDER="1" CELLBORDER="1" CELLSPACING="0" CELLPADDING="6">
       <TR><TD BGCOLOR="#ffffe0"><B>DS18B20 (ESC)</B></TD></TR>
-      <TR><TD PORT="vcc">VDD (Rot)</TD></TR>
-      <TR><TD PORT="gnd">GND (Schwarz)</TD></TR>
+      <TR><TD PORT="vcc">VDD (3.3V via WAGO)</TD></TR>
+      <TR><TD PORT="gnd">GND</TD></TR>
       <TR><TD PORT="dq">Data (Gelb/Blau)</TD></TR>
     </TABLE>>'''
 
     hall_html = '''<
     <TABLE BORDER="1" CELLBORDER="1" CELLSPACING="0" CELLPADDING="6">
       <TR><TD BGCOLOR="#ffb6c1"><B>A3144 Hall-Sensor</B></TD></TR>
-      <TR><TD PORT="vcc">1: VCC (3.3V)</TD></TR>
+      <TR><TD PORT="vcc">1: VCC (3.3V via WAGO)</TD></TR>
       <TR><TD PORT="gnd">2: GND</TD></TR>
       <TR><TD PORT="out">3: DOUT (Signal)</TD></TR>
     </TABLE>>'''
@@ -88,16 +86,18 @@ def erstelle_perfekten_offline_loetplan():
     res_html = '''<
     <TABLE BORDER="1" CELLBORDER="1" CELLSPACING="0" CELLPADDING="4">
       <TR><TD COLSPAN="2" BGCOLOR="white"><B>4.7 kΩ Pull-Up</B></TD></TR>
-      <TR><TD PORT="p1">Zu 3.3V</TD><TD PORT="p2">Zu GPIO 4</TD></TR>
+      <TR><TD PORT="p1">Zu 3.3V WAGO</TD><TD PORT="p2">Zu GPIO 4</TD></TR>
     </TABLE>>'''
 
     with dot.subgraph() as s_right:
         s_right.attr(rank='same')
+        s_right.node('SD', label=sd_html)
         s_right.node('GPS', label=gps_html)
         s_right.node('TEMP_MOT', label=temp_mot_html)
         s_right.node('TEMP_ESC', label=temp_esc_html)
         s_right.node('HALL', label=hall_html)
         s_right.node('RES', label=res_html)
+        s_right.edge('SD', 'GPS', style='invis')
         s_right.edge('GPS', 'TEMP_MOT', style='invis')
         s_right.edge('TEMP_MOT', 'TEMP_ESC', style='invis')
         s_right.edge('TEMP_ESC', 'HALL', style='invis')
@@ -105,49 +105,48 @@ def erstelle_perfekten_offline_loetplan():
 
     # --- VERKABELUNG ---
 
-    # LINKE SEITE -> Andocken an ESP Linke Pins (:w)
-    
-    # RC Empfänger -> ESP32 (Stromversorgung)
+    # LINKE SEITE -> Strom von RC zu ESP
     dot.edge('RX:v5:e', 'ESP:vin:w', color='red', penwidth='3', dir='none')
-    dot.edge('RX:gnd:e', 'ESP:gnd_l1:w', color='black', penwidth='3', dir='none')
+    dot.edge('RX:gnd:e', 'ESP:gnd_l:w', color='black', penwidth='3', dir='none')
     
-    # SD Karte (3.3V gestrichelt vom ESP!)
-    dot.edge('SD:vcc:e', 'ESP:3v3_l:w', color='red', style='dashed', penwidth='2', dir='none')
-    dot.edge('SD:gnd:e', 'ESP:gnd_l2:w', color='black', penwidth='2', dir='none')
-    dot.edge('SD:mosi:e', 'ESP:g23:w', color='blue', penwidth='2', dir='none')
-    dot.edge('SD:miso:e', 'ESP:g19:w', color='blue', penwidth='2', dir='none')
-    dot.edge('SD:sck:e', 'ESP:g18:w', color='blue', penwidth='2', dir='none')
-    dot.edge('SD:cs:e', 'ESP:g5:w', color='blue', penwidth='2', dir='none')
-
-    # RECHTE SEITE -> Andocken an ESP Rechte Pins (:e)
+    # RECHTE SEITE -> Daten & 3.3V Routing
+    
+    # SD Karte (ACHTUNG: VCC geht an 5V / VIN!)
+    dot.edge('ESP:vin', 'SD:vcc:w', color='red', penwidth='3', dir='none')
+    dot.edge('ESP:gnd_r:e', 'SD:gnd:w', color='black', penwidth='2', dir='none')
+    dot.edge('ESP:d23:e', 'SD:mosi:w', color='blue', penwidth='2', dir='none')
+    dot.edge('ESP:d19:e', 'SD:miso:w', color='blue', penwidth='2', dir='none')
+    dot.edge('ESP:d18:e', 'SD:sck:w', color='blue', penwidth='2', dir='none')
+    dot.edge('ESP:d5:e', 'SD:cs:w', color='blue', penwidth='2', dir='none')
 
     # GPS (UART Überkreuzt)
-    dot.edge('ESP:3v3_r1:e', 'GPS:vcc:w', color='red', style='dashed', penwidth='2', dir='none')
-    dot.edge('ESP:gnd_r1:e', 'GPS:gnd:w', color='black', penwidth='2', dir='none')
-    dot.edge('ESP:g16:e', 'GPS:tx:w', color='magenta', penwidth='2', dir='none') 
-    dot.edge('ESP:g17:e', 'GPS:rx:w', color='purple', penwidth='2', dir='none')  
+    dot.edge('ESP:3v3:e', 'GPS:vcc:w', color='red', style='dashed', penwidth='2', dir='none')
+    dot.edge('ESP:gnd_r:e', 'GPS:gnd:w', color='black', penwidth='2', dir='none')
+    dot.edge('ESP:rx2:e', 'GPS:tx:w', color='magenta', penwidth='2', dir='none') 
+    dot.edge('ESP:tx2:e', 'GPS:rx:w', color='purple', penwidth='2', dir='none')  
 
     # Motor Temp
-    dot.edge('ESP:3v3_r2:e', 'TEMP_MOT:vcc:w', color='red', style='dashed', penwidth='2', dir='none')
-    dot.edge('ESP:gnd_r2:e', 'TEMP_MOT:gnd:w', color='black', penwidth='2', dir='none')
-    dot.edge('ESP:g4:e', 'TEMP_MOT:dq:w', color='orange', penwidth='2', dir='none')
+    dot.edge('ESP:3v3:e', 'TEMP_MOT:vcc:w', color='red', style='dashed', penwidth='2', dir='none')
+    dot.edge('ESP:gnd_r:e', 'TEMP_MOT:gnd:w', color='black', penwidth='2', dir='none')
+    dot.edge('ESP:d4:e', 'TEMP_MOT:dq:w', color='orange', penwidth='2', dir='none')
 
     # ESC Temp
-    dot.edge('ESP:3v3_r2:e', 'TEMP_ESC:vcc:w', color='red', style='dashed', penwidth='2', dir='none')
-    dot.edge('ESP:gnd_r2:e', 'TEMP_ESC:gnd:w', color='black', penwidth='2', dir='none')
-    dot.edge('ESP:g4:e', 'TEMP_ESC:dq:w', color='orange', penwidth='2', dir='none')
+    dot.edge('ESP:3v3:e', 'TEMP_ESC:vcc:w', color='red', style='dashed', penwidth='2', dir='none')
+    dot.edge('ESP:gnd_r:e', 'TEMP_ESC:gnd:w', color='black', penwidth='2', dir='none')
+    dot.edge('ESP:d4:e', 'TEMP_ESC:dq:w', color='orange', penwidth='2', dir='none')
 
     # Hall Sensor
-    dot.edge('ESP:3v3_r2:e', 'HALL:vcc:w', color='red', style='dashed', penwidth='2', dir='none')
-    dot.edge('ESP:gnd_r2:e', 'HALL:gnd:w', color='black', penwidth='2', dir='none')
-    dot.edge('ESP:g2:e', 'HALL:out:w', color='green', penwidth='2', dir='none')
+    dot.edge('ESP:3v3:e', 'HALL:vcc:w', color='red', style='dashed', penwidth='2', dir='none')
+    dot.edge('ESP:gnd_r:e', 'HALL:gnd:w', color='black', penwidth='2', dir='none')
+    dot.edge('ESP:d2:e', 'HALL:out:w', color='green', penwidth='2', dir='none')
 
     # Pull-Up Widerstand
-    dot.edge('ESP:3v3_r2:e', 'RES:p1:w', color='red', style='dashed', penwidth='2', dir='none')
-    dot.edge('RES:p2:w', 'ESP:g4:e', color='orange', style='dashed', penwidth='2', dir='none')
+    dot.edge('ESP:3v3:e', 'RES:p1:w', color='red', style='dashed', penwidth='2', dir='none')
+    dot.edge('RES:p2:w', 'ESP:d4:e', color='orange', style='dashed', penwidth='2', dir='none')
 
     dot.render(view=False)
-    print("Offline-Lötplan (ohne LTE, mit RC-Stromversorgung) generiert!")
+    print("Aktualisierter 30-Pin Lötplan (SD-Karte an 5V!) generiert!")
 
 if __name__ == '__main__':
-    erstelle_perfekten_offline_loetplan()
+    erstelle_perfekten_offline_loetplan_30pin()
+    
